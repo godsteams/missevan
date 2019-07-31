12dfc6a4ab944ced42f57526b5c08c97dc1e9b9b
mecin <mecin@foxmail.com>
2019-07-31 20:45:14 +0800

    第二次提交

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,7 +1,38 @@
 <template>
     <div>
         <commonhead/>
-        <h1>登录</h1>
+        <div class="container">
+            <div class="main-page">
+                <form action="" method="post">
+                    <span>登录 猫耳FM</span>
+                    <input type="text" name="username" placeholder="手机号/邮箱">
+                    <div class="pwd">
+                        <input type="password" name="pwd" placeholder="密码">
+                        <!-- 忘记密码 -->
+                        <a href="">忘记密码?</a>
+                    </div>
+                    <div>
+                        <span>
+                            <input type="checkbox" value="1" id="check-row-before">
+                            <label for="check-row-before">记住我</label>
+                        </span>
+                        <span>海外手机号登录</span>
+                    </div>
+                    <input type="submit" value="登录">
+                    <router-link to="/register">注册</router-link>
+                </form>
+                <div class="threelogin">
+                        <div class="line-left line"></div>
+                        <div class="line-right line"></div>
+                        <div>第三方登录</div>
+                </div>
+                <div class="threeimg">
+                    <a href="https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=101117657&redirect_uri=https%3A%2F%2Fwww.missevan.com%2Fmember%2Fauthqq%3Fbackurl=https%3A%2F%2Fm.missevan.com%2F" class="qqlogin"></a>
+                    <a href="https://api.weibo.com/oauth2/authorize?client_id=4018040466&response_type=code&redirect_uri=https%3A%2F%2Fwww.missevan.com%2Fmember%2Fauthweibo%3Fbackurl=https%3A%2F%2Fm.missevan.com%2F" class="weibo"></a>
+                    <a href="" class="bilibli"></a>
+                </div>
+            </div>
+        </div>
         <commonfoot/>
     </div>
 </template>
@@ -14,3 +45,6 @@ export default {
     }
 }
 </script>
+<style scoped>
+@import url("../assets/css/login.css");
+</style>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,22 @@
 import VueRouter from "vue-router";
 import search from "./pages/search.vue"
 import commonhead from "./components/commonHead.vue"
+import common from "./components/common.vue"
 import login from "./pages/login.vue"
+// 引入推荐组件
+
+// 引入分类组件
+
+// 引入音单组件
+
 const router = new VueRouter({
     routes:[
-        {path:"/",component:commonhead},
+        {path:"/",component:common},
         {name:"search",path:"/search",component:search},
-        {path:"/login",component:login}
+        {path:"/login",component:login},
+        // {path:"/musiclist",component: }
+        // {path:"/recomd",component: }
+        // {path:"/classify",component: }
     ]
 })
 
